1565f2e47ecdde66af53f10dcdc55d270b363304
Henrique de Paula <henrique.depaula@hotmail.com>
2018-03-28 21:57:20 -0300

    Problematica calendario e projeto

--- myapplication12/src/main/java/com/example/myapplication12/MainActivity.java
+++ myapplication12/src/main/java/com/example/myapplication12/MainActivity.java
@@ -11,11 +11,22 @@ public class MainActivity extends Activity {
     private EditText editProjeto;
     private TextView textDataInicio;
     private TextView textDataFinal;
+    private TextView projetoResult;
+    private TextView finalizadoResult;
     private CheckBox checkFinalizado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editProjeto.findViewById(R.id.editProjeto);
+        textDataInicio.findViewById(R.id.textDataInicio);
+        textDataFinal.findViewById(R.id.textDataFinal);
+        projetoResult.findViewById(R.id.projetoResult);
+        finalizadoResult.findViewById(R.id.finalizadoResult);
+        checkFinalizado.findViewById(R.id.checkFinalizado);
+
     }
 }
