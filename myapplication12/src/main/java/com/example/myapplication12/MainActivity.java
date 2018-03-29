383f36ad404fc4c5868fde088a3fddeb8381b360
Henrique de Paula <henrique.depaula@hotmail.com>
2018-03-28 22:03:45 -0300

    problematica calendario 1.02

--- myapplication12/src/main/java/com/example/myapplication12/MainActivity.java
+++ myapplication12/src/main/java/com/example/myapplication12/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.myapplication12;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
@@ -27,6 +32,27 @@ public class MainActivity extends Activity {
         projetoResult.findViewById(R.id.projetoResult);
         finalizadoResult.findViewById(R.id.finalizadoResult);
         checkFinalizado.findViewById(R.id.checkFinalizado);
+
+        final Calendar calendarioInicio = Calendar.getInstance();
+
+        final Calendar calendarioFinal = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                calendarioInicio.set(Calendar.YEAR,year);
+                calendarioInicio.set(Calendar.MONTH,monthOfYear);
+                calendarioInicio.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+                textDataInicio.setText(format.format(calendarioInicio.getTime()));
+
+
+            }
+        };
+
 
     }
 }
